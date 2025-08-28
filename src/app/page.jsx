92ab0eab1e50ad43d327fa9b0d8ce98da0adc724
lilo-error404 <murilosouzagames2005@gmail.com>
2025-08-28 11:19:08 -0300

    Criando componente Header, Footer e atualização da Home

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Header from '../app/components/Header'
+import Footer from '../app/components/Footer'
+
+export default function Home() {
+  return (
+   <Header />
+
+  //  <Footer />
+  );
+}
