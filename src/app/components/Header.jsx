28babc9a56879555255226550021efe20e8cf910
lilo-error404 <murilosouzagames2005@gmail.com>
2025-08-29 10:14:55 -0300

    Header pronto

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,20 +1,33 @@
 export default function Header() {
     return (
-        <header>
-            <nav>
-                <ul className=" font-sans flex flex-row bg-white p-5">
-                    <div><li className=""><a href="/"><img src="logo.png" alt="" className="h-10 w-10" /></a></li></div>
-                    <li className="text-gray-900 cursor-pointer hover:text-gray-700">Why Grammarly</li>
-                    <li className="text-gray-900 cursor-pointer hover:text-gray-700">For Work</li>
-                    <li className="text-gray-900 cursor-pointer hover:text-gray-700">For Education</li>
-                    <li className="text-gray-900 cursor-pointer hover:text-gray-700">Compare Plans</li>
-                    <li className="text-gray-900 cursor-pointer hover:text-gray-700">Tools & Guides</li>
-                    <div className="flex justify-end">
-                        <li className="text-black cursor-pointer font-bold">My Grammarly</li>
-                        <li><a href="/free" className="bg-green-800 hover:bg-green-700 p-2 rounded-lg font-bold"> Start a Free Trial</a></li>
-                    </div>
+        <header className="font-sans">
+            <nav className="flex flex-row justify-between bg-white">
+                <a href="/">
+                    <div className=" flex flex-row items-center"><img src="logo.png" alt="logo" className="h-15 w-15 ms-10" /> <p className="text-black font-bold text-xl">grammarly</p></div>
+                </a>
+
+                <ul className="flex flex-row items-center">
+                    <a><li className="text-gray-900 cursor-pointer hover:text-gray-700 me-10">Why Grammarly</li></a>
+                    <a><li className="text-gray-900 cursor-pointer hover:text-gray-700 me-10">For Work</li></a>
+                    <a><li className="text-gray-900 cursor-pointer hover:text-gray-700 me-10">For Education</li></a>
+                    <a><li className="text-gray-900 cursor-pointer hover:text-gray-700 me-10">Compare Plans</li></a>
+                    <a><li className="text-gray-900 cursor-pointer hover:text-gray-700 me-10">Tools & Guides</li></a>
                 </ul>
+                <div className="flex flex-row me-10 items-center">
+                    <a href=""><div className="text-black cursor-pointer font-bold content-center me-5">My Grammarly</div></a>
+                    <button className="bg-green-800 hover:bg-green-900 font-bold rounded-xl w-40 h-10">Start a Free Trial</button>
+                </div>
             </nav>
-        </header>
+
+            <div className="grid grid-cols-3 bg-gray-200 p-5 items-center">
+                <h1 className="font-bold text-2xl text-black">Business</h1>
+                <ul className="flex flex-row justify-around">
+                    <li className="text-gray-900">Pricing</li>
+                    <li className="text-gray-900">Solutions</li>
+                    <li className="text-gray-900">Learn</li>
+                    <li className="text-gray-900">Security</li>
+                </ul>
+            </div>
+        </header >
     )
 };
