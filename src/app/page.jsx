a3d3d22fccfb6a6c0118f15d8fe55673278b1e36
lilo-error404 <murilosouzagames2005@gmail.com>
2025-08-29 11:12:05 -0300

    falta terminar a Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,13 @@ export default function Home() {
     <>
       <Header />
       <section className='bg-[#0E1019] container mx-auto font-sans flex flex-row'>
-        <div>
+        <div className=''>
         <h1 className="text-4xl text-white font-bold">
         AI Your Business Can Run With
         </h1>
-          <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam! Possimus, ipsa suscipit. Consequatur amet, iste assumenda fuga veritatis nobis itaque quos eius nulla doloribus quas eaque. Quidem, facilis molestias?</p>
+          <p className="text-white mt-9 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam! Possimus, ipsa suscipit. Consequatur amet, iste assumenda fuga veritatis nobis itaque quos eius nulla doloribus quas eaque. Quidem, facilis molestias?</p>
         </div>
-      <img src="guy.png" alt="guy" />
+      <img src="guy.png" alt="guy" className='w-200 h-150'/>
       </section>
 
  {/* <Footer /> */}
